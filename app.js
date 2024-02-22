b49d94bc03f460f8d61af98ecb1999a15bd8f718
asiftariq1 <aasifakakll25@gmail.com>
2024-02-22 13:22:59 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,165 @@
+// Chapter No:1
+
+// Q no:1
+
+// alert()
+
+// Q no:2
+
+// alert("Error!please enter a valid password")
+
+// Q no:3
+
+// alert("Wellcome to js land... \n Happy coding")
+
+// Q no:4
+
+// alert("Wellcome to js land...")
+// aalert("Happy coding")
+
+// Q no:5
+
+// alert("Hello... I can Js though my web browser's console")
+
+// Q no:6
+
+// Q no:7
+
+
+// Chapter No: 2
+
+// Q No:1
+
+// var username =
+
+// Q No:2
+
+// var myname = asif tariq
+
+// Q No:3
+
+// var message = Hello world
+// alert(message)
+
+// Q No:4
+
+// alert("John Doe")
+// alert("15 years old")
+// alert("certified web development")
+
+// Q No:5
+
+// var msg = Plzzz
+// var msg1 = Plzz
+// var msg2 = Plz
+// var msg3 = Pl
+// alert(msg1 + msg2 + msg3 + msg)
+
+// Q No:6
+
+// var email = asifakakll25@gmail.com
+// alert("email")
+
+// Q No:7
+
+// var book = "A smarter way to learn js"
+// alert(book)
+
+// Q No:8
+
+// document.write("Yah! I can write html though vs code")
+
+// Q No:9
+ 
+// alert("----------------------")
+
+
+// Chapter No: 3
+
+// Q no:1
+
+// var age = "I am 15 years old"
+// alert("age")
+
+// Q no:2
+
+// alert(you visited 23 times)
+
+// Q no:3
+
+// var birth = "my birth date is 1998"
+// document.write(birth)
+
+// Q no:4
+
+// var name = john doe
+// var title = 5 T-shirt
+// document.write(name "ordered" title "total")
+
+// chapter No:1
+
+// Q no:1
+
+// alert("ASif");
+// alert("tariq");
+// alert("asifakakll25@gmail.com");
+// alert("563343");
+
+// Q no:2
+
+// alert("you're learning JavaScript!")
+
+// Q no:3
+
+// alert("you're learning JavaScript!")
+
+
+// chapter NO:2
+
+// Q no:1
+
+// var me = "asif"
+
+// Q no:2
+
+// var me = "asif"
+
+// Q no:3
+
+// var teamname = asif kaif ali
+// alert("teamname")
+
+// Q no:4
+
+// var bestman = "charlie champman";
+
+
+// chapter NO:3
+
+// Q no:1
+
+// vaar "caseQty" =
+
+// Q no:2
+
+// var caseQty = 144;
+
+// Q no:3
+
+// var num = 9;
+
+// Q no:4
+
+// var num = 2;
+
+// Q no:5
+
+// var merchtotal = 100;
+// var shippingcharge = 10;
+// var ordertotal = merchtotal + shippingcharge;
+
+// Q no:6
+
+// var val = 15;
+// var override vall = val + 10;
+
